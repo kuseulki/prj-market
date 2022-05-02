5033ea71be098c4e3d6212d0a8167e4fb16c6a0e
kuseulki <68773294+kuseulki@users.noreply.github.com>
2022-05-02 16:17:34 +0900

    Delete OrderRepository.java

--- src/main/java/com/hellomarket/repository/OrderRepository.java
+++
@@ -1,8 +0,0 @@
-//package com.hellomarket.repository;
-//
-//import com.hellomarket.entity.Order;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface OrderRepository extends JpaRepository<Order, Long> {
-//
-//}
