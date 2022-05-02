17ed5b6995fd103e488d0983e07c10ee13f086a5
kuseulki <68773294+kuseulki@users.noreply.github.com>
2022-05-02 16:17:08 +0900

    Delete CartDetailDtoRepository.java

--- src/main/java/com/hellomarket/repository/CartDetailDtoRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.hellomarket.repository;
-//
-//import com.hellomarket.dto.CartDetailDto;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface CartDetailDtoRepository extends JpaRepository<CartDetailDto, Long> {
-//}
